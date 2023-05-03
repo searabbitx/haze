35d7c548da8e119423f2df4eb788e50e331df2ed
KS <kamsolecki@gmail.com>
2023-05-03 14:12:02 -0400

    Verify file existence

--- main.go
+++ main.go
@@ -10,6 +10,12 @@ func main() {
 		fmt.Println("Wrong number of arguments.")
 		os.Exit(1)
 	}
+
 	rq := os.Args[1]
+	if _, err := os.Stat(rq); err != nil {
+		fmt.Println("Cannot read", rq);
+		os.Exit(1)
+	}
+
 	fmt.Println("Request file:", rq)
 }
