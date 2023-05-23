19df2e6c9eb65d987256fbd8208d8134f81abce8
KS <kamsolecki@gmail.com>
2023-05-23 11:33:29 -0400

    Implement arithmetic mutations

--- mutation/mutation.go
+++ mutation/mutation.go
@@ -25,8 +25,29 @@ func SstiFuzz(rq http.Request, mutable Mutable) []http.Request {
 	return mutable(rq, trans)
 }
 
+func Negative(rq http.Request, mutable Mutable) []http.Request {
+	trans := func(val string) string {
+		return "-" + val
+	}
+	return mutable(rq, trans)
+}
+
+func MinusOne(rq http.Request, mutable Mutable) []http.Request {
+	trans := func(val string) string {
+		return val + "-1"
+	}
+	return mutable(rq, trans)
+}
+
+func TimesSeven(rq http.Request, mutable Mutable) []http.Request {
+	trans := func(val string) string {
+		return val + "*7"
+	}
+	return mutable(rq, trans)
+}
+
 func AllMutations() []Mutation {
-	return []Mutation{SingleQuotes, DoubleQuotes, SstiFuzz}
+	return []Mutation{SingleQuotes, DoubleQuotes, SstiFuzz, Negative, MinusOne, TimesSeven}
 }
 
 func Mutate(rq http.Request, mutations []Mutation, mutables []Mutable) []http.Request {
