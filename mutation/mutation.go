b9222c893bc8b4e6ec4c0002c2aabeeb498f7470
KS <kamsolecki@gmail.com>
2023-05-26 08:57:27 -0400

    Refactor mutations

--- mutation/mutation.go
+++ mutation/mutation.go
@@ -5,49 +5,58 @@ import "github.com/kamil-s-solecki/haze/http"
 type Mutation func(http.Request, Mutable) []http.Request
 
 func SingleQuotes(rq http.Request, mutable Mutable) []http.Request {
-	trans := func(val string) string {
-		return val + "'"
-	}
-	return mutable(rq, trans)
+	return suffixMutation(rq, mutable, "'")
 }
 
 func DoubleQuotes(rq http.Request, mutable Mutable) []http.Request {
-	trans := func(val string) string {
-		return val + "\""
-	}
-	return mutable(rq, trans)
+	return suffixMutation(rq, mutable, "\"")
 }
 
 func SstiFuzz(rq http.Request, mutable Mutable) []http.Request {
-	trans := func(val string) string {
-		return val + "${{<%[%'\"}}%\\."
-	}
-	return mutable(rq, trans)
+	return suffixMutation(rq, mutable, "${{<%[%'\"}}%\\.")
 }
 
 func Negative(rq http.Request, mutable Mutable) []http.Request {
-	trans := func(val string) string {
-		return "-" + val
-	}
-	return mutable(rq, trans)
+	return prefixMutation(rq, mutable, "-")
 }
 
 func MinusOne(rq http.Request, mutable Mutable) []http.Request {
+	return suffixMutation(rq, mutable, "-1")
+}
+
+func TimesSeven(rq http.Request, mutable Mutable) []http.Request {
+	return suffixMutation(rq, mutable, "*7")
+}
+
+func Brackets(rq http.Request, mutable Mutable) []http.Request {
+	return suffixMutation(rq, mutable, ")]}>")
+}
+
+func Backtick(rq http.Request, mutable Mutable) []http.Request {
+	return suffixMutation(rq, mutable, "`")
+}
+
+func Comma(rq http.Request, mutable Mutable) []http.Request {
+	return suffixMutation(rq, mutable, ",")
+}
+
+func suffixMutation(rq http.Request, mutable Mutable, suffix string) []http.Request {
 	trans := func(val string) string {
-		return val + "-1"
+		return val + suffix
 	}
 	return mutable(rq, trans)
 }
 
-func TimesSeven(rq http.Request, mutable Mutable) []http.Request {
+func prefixMutation(rq http.Request, mutable Mutable, prefix string) []http.Request {
 	trans := func(val string) string {
-		return val + "*7"
+		return prefix + val
 	}
 	return mutable(rq, trans)
 }
 
 func AllMutations() []Mutation {
-	return []Mutation{SingleQuotes, DoubleQuotes, SstiFuzz, Negative, MinusOne, TimesSeven}
+	return []Mutation{SingleQuotes, DoubleQuotes, SstiFuzz, Negative, MinusOne,
+		TimesSeven, Brackets, Backtick, Comma}
 }
 
 func Mutate(rq http.Request, mutations []Mutation, mutables []Mutable) []http.Request {
