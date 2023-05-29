3d3e707224c7b9913284c72205a5397dfca5962a
KS <kamsolecki@gmail.com>
2023-05-29 10:42:24 -0400

    Add a progress bar

---
+++ progress/progress.go
@@ -0,0 +1,37 @@
+package progress
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+type Bar struct {
+	curr, total int
+	buff        *bufio.Writer
+}
+
+func Start(total int) Bar {
+	b := Bar{curr: 0, total: total, buff: bufio.NewWriter(os.Stdout)}
+	return b
+}
+
+func (b *Bar) Next() {
+	b.curr++
+	b.update()
+}
+
+func (b Bar) update() {
+	defer b.buff.Flush()
+	fmt.Fprint(b.buff, "\033[30D\033[0K", b, "\033[30D")
+}
+
+const spinChars = `|/-\`
+
+func (b Bar) spinner() byte {
+	return spinChars[b.curr%len(spinChars)]
+}
+
+func (b Bar) String() string {
+	return fmt.Sprintf("  %c [ %v / %v ] %c", b.spinner(), b.curr, b.total, b.spinner())
+}
--- var/test.php
+++ var/test.php
@@ -1,5 +1,6 @@
 <?php
 
+usleep(300 * 1000);
 if (isset($_GET['foo']) && $_GET['foo'] == "bar'") {
     http_response_code(500);
     echo "Error!";
