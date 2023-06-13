3f9f293c3240c5751c7d203589280af4f7c5be05
KS <kamsolecki@gmail.com>
2023-06-13 08:35:53 -0400

    Move the remaining prints to the gui module

--- gui/gui.go
+++ gui/gui.go
@@ -5,18 +5,22 @@ import (
 	"fmt"
 	"github.com/kamil-s-solecki/haze/http"
 	"github.com/kamil-s-solecki/haze/progress"
+	"log"
 	"os"
 	"sync"
 )
 
 type Gui struct {
-	buff *bufio.Writer
-	mu   sync.Mutex
+	buff     *bufio.Writer
+	mu       sync.Mutex
+	errorLog *log.Logger
 }
 
 func Create() Gui {
-	g := Gui{buff: bufio.NewWriter(os.Stdout)}
-	return g
+	return Gui{
+		buff:     bufio.NewWriter(os.Stdout),
+		errorLog: log.New(os.Stdout, "ERROR: ", 0),
+	}
 }
 
 func (g Gui) FuzzNewFile(rfile string) {
@@ -28,8 +32,24 @@ func (g Gui) FuzzNewRequest(rq http.Request) {
 	g.printf("      ---\n")
 }
 
+func (g Gui) Crash(res http.Response, fname string) {
+	g.printf("    ! Crash:       %s (%s)\n", res, fname)
+}
+
+func (g Gui) Probe(probe http.Response) {
+	g.printf("      Probe:      %v\n", probe)
+}
+
 func (g Gui) EmptyLine() {
 	g.printf("\n")
+}
+
+func (g Gui) Fatal(err error) {
+	g.errorLog.Fatal(err)
+}
+
+func (g Gui) Error(err error) {
+	g.errorLog.Println(err)
 }
 
 func (g Gui) printf(format string, a ...any) {
--- main.go
+++ main.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"fmt"
-	"log"
 	"os"
 	"github.com/kamil-s-solecki/haze/cliargs"
 	"github.com/kamil-s-solecki/haze/http"
@@ -14,57 +12,9 @@ import (
 	"github.com/kamil-s-solecki/haze/gui"
 )
 
-var ErrorLog *log.Logger
 var agui gui.Gui
 
-func readRawRequest(rqPath string) []byte {
-	rawRq, _ := os.ReadFile(rqPath)
-	return rawRq
-}
-
-func probe(rq http.Request, addr string) {
-	probe, err := rq.Send(addr)
-	if err != nil {
-		ErrorLog.Fatal(err)
-	}
-	fmt.Println("      Probe:      ", probe)
-}
-
-func fuzz(args cliargs.Args, rq http.Request, reportDir string) {
-	matchers, filters := reportable.FromArgs(args)
-	muts := mutation.Mutate(rq, mutation.AllMutations(), mutable.AllMutatables())
-	bar := agui.ProgressBar(len(muts))
-	pool := workerpool.NewPool(args.Threads)
-
-	for  _, mut := range muts {
-		mut := mut
-		task := func() {
-			res, err := mut.Send(args.Host)
-			if err != nil {
-				ErrorLog.Println(err)
-			}
-			if reportable.IsReportable(res, matchers, filters) {
-				fname := report.Report(mut.Raw(args.Host), res.Raw, reportDir)
-				bar.Log(fmt.Sprintf("    ! Crash:       %s (%s)\n", res, fname))
-			}
-			bar.Next()
-		}
-		pool.RunTask(task)
-	}
-	pool.Wait()
-	bar.End()
-}
-
-func parseRequestsFromFile(rfile string, args cliargs.Args) []http.Request {
-	raw := readRawRequest(rfile)
-	if !args.Har {
-		return []http.Request{http.Parse(raw)}
-	}
-	return http.ParseHar(raw, args.Host)
-}
-
 func main() {
-	ErrorLog = log.New(os.Stdout, "ERROR: ", 0)
 	agui = gui.Create()
 	args := cliargs.ParseArgs()
 	http.SetupTransport(args.Proxy)
@@ -87,4 +37,50 @@ func main() {
 			}
 		}
 	}
+}
+
+func parseRequestsFromFile(rfile string, args cliargs.Args) []http.Request {
+	raw := readRawRequest(rfile)
+	if !args.Har {
+		return []http.Request{http.Parse(raw)}
+	}
+	return http.ParseHar(raw, args.Host)
+}
+
+func readRawRequest(rqPath string) []byte {
+	rawRq, _ := os.ReadFile(rqPath)
+	return rawRq
+}
+
+func probe(rq http.Request, addr string) {
+	probe, err := rq.Send(addr)
+	if err != nil {
+		agui.Fatal(err)
+	}
+	agui.Probe(probe)
+}
+
+func fuzz(args cliargs.Args, rq http.Request, reportDir string) {
+	matchers, filters := reportable.FromArgs(args)
+	muts := mutation.Mutate(rq, mutation.AllMutations(), mutable.AllMutatables())
+	bar := agui.ProgressBar(len(muts))
+	pool := workerpool.NewPool(args.Threads)
+
+	for  _, mut := range muts {
+		mut := mut
+		task := func() {
+			res, err := mut.Send(args.Host)
+			if err != nil {
+				agui.Error(err)
+			}
+			if reportable.IsReportable(res, matchers, filters) {
+				fname := report.Report(mut.Raw(args.Host), res.Raw, reportDir)
+				agui.Crash(res, fname)
+			}
+			bar.Next()
+		}
+		pool.RunTask(task)
+	}
+	pool.Wait()
+	bar.End()
 }
