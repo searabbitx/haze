826b2674f839a2d65764a78e148c67c23758ab10
KS <46087838+kamil-s-solecki@users.noreply.github.com>
2023-09-30 10:31:14 -0400

    Move urlEncode function to utils

--- mutable/mutable.go
+++ mutable/mutable.go
@@ -2,26 +2,11 @@ package mutable
 
 import (
 	"github.com/kamil-s-solecki/haze/http"
-	"strings"
 )
 
 type Mutable struct {
 	Name  string
 	Apply func(http.Request, func(string) string) []http.Request
-}
-
-func urlEncodeSpecials(val string) string {
-	val = strings.Replace(val, "%", "%25", -1)
-	val = strings.Replace(val, "\\", "%5c", -1)
-	val = strings.Replace(val, "\"", "%22", -1)
-	val = strings.Replace(val, "\x00", "%00", -1)
-	val = strings.Replace(val, " ", "%20", -1)
-	val = strings.Replace(val, "\t", "%09", -1)
-	val = strings.Replace(val, "\f", "%0c", -1)
-	val = strings.Replace(val, "\r", "%0d", -1)
-	val = strings.Replace(val, "\n", "%0a", -1)
-	val = strings.Replace(val, ";", "%3b", -1)
-	return val
 }
 
 func AllMutatables() []Mutable {
--- mutable/path.go
+++ mutable/path.go
@@ -2,12 +2,13 @@ package mutable
 
 import (
 	"github.com/kamil-s-solecki/haze/http"
+	"github.com/kamil-s-solecki/haze/utils"
 )
 
 var Path = Mutable{"Path", path}
 
 func path(rq http.Request, trans func(string) string) []http.Request {
 	noLeadingSlash := rq.Path[1:]
-	val := urlEncodeSpecials(trans(noLeadingSlash))
+	val := utils.UrlEncodeSpecials(trans(noLeadingSlash))
 	return []http.Request{rq.WithPath("/" + val)}
 }
