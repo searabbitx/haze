ec543dcb9ab16e6f94b11221e15102a6f3244391
searabbit <46087838+searabbitx@users.noreply.github.com>
2024-09-07 18:15:43 +0200

    Fix mutex usage

--- tui/tui.go
+++ tui/tui.go
@@ -23,36 +23,36 @@ func Create() Tui {
 	}
 }
 
-func (t Tui) FuzzNewFile(rfile string) {
+func (t *Tui) FuzzNewFile(rfile string) {
 	t.printf("... ( %v ) ...\n", rfile)
 }
 
-func (t Tui) FuzzNewRequest(rq http.Request) {
+func (t *Tui) FuzzNewRequest(rq http.Request) {
 	t.printf("      %v %v\n", rq.Method, rq.RequestUri)
 	t.printf("      ---\n")
 }
 
-func (t Tui) Crash(res http.Response, fname string) {
+func (t *Tui) Crash(res http.Response, fname string) {
 	t.printf("    ! Crash:       %s (%s)\n", res, fname)
 }
 
-func (t Tui) Probe(probe http.Response) {
+func (t *Tui) Probe(probe http.Response) {
 	t.printf("      Probe:      %v\n", probe)
 }
 
-func (t Tui) EmptyLine() {
+func (t *Tui) EmptyLine() {
 	t.printf("\n")
 }
 
-func (t Tui) Fatal(err error) {
+func (t *Tui) Fatal(err error) {
 	t.errorLog.Fatal(err)
 }
 
-func (t Tui) Error(err error) {
+func (t *Tui) Error(err error) {
 	t.errorLog.Println(err)
 }
 
-func (t Tui) printf(format string, a ...any) {
+func (t *Tui) printf(format string, a ...any) {
 	defer t.mu.Unlock()
 	defer t.buff.Flush()
 	t.mu.Lock()
@@ -60,6 +60,6 @@ func (t Tui) printf(format string, a ...any) {
 	fmt.Fprintf(t.buff, format, a...)
 }
 
-func (t Tui) ProgressBar(total int) progress.Bar {
-	return progress.Start(total, t.buff, t.mu)
+func (t *Tui) ProgressBar(total int) progress.Bar {
+	return progress.Start(total, t.buff, &t.mu)
 }
