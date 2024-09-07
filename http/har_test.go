84a7a3049a909dcc31f76b9451814446f6acc7e9
searabbit <46087838+searabbitx@users.noreply.github.com>
2024-09-07 18:15:43 +0200

    Create the parse har method

---
+++ http/har_test.go
@@ -0,0 +1,23 @@
+package http
+
+import (
+	"github.com/kamil-s-solecki/haze/testutils"
+	"os"
+	"testing"
+)
+
+func readHar(path string) []byte {
+	content, err := os.ReadFile(path)
+	if err != nil {
+		panic(err)
+	}
+	return content
+}
+
+func TestReturnEmptyArrayForNoRequestsInHar(t *testing.T) {
+	har := readHar("../var/hars/empty.har")
+
+	got := ParseHar(har)
+
+	testutils.AssertLen(t, got, 0)
+}
